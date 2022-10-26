47fe9cf0740e876bf3906398621f2c0671a6a4f7
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-10-26 23:37:42 +0630

    Update Enchanted_Project.py

--- Enchanted_Project.py
+++ Enchanted_Project.py
@@ -77,7 +77,8 @@ def color_analysis(img):
 modified_image = prep_image(image1)
 color_analysis(modified_image)
 
-st.image("Midnights-Logo.png", width=100)
+st.image("Midnights-Logo.png", width=200)
+st.image("TS-Midnights-Logo.PNG", width=200)
 #st.image("Moon Stone Blue Vinyl.png", caption="Original Photo", width=240, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 #st.image("color_analysis_report.png", caption="Color Analysis Pie Chart", width=1080, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 col1, col2= st.columns((2,1))
