98eb194e35e16e6ab2a66e08b59964e8770894c9
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-10-27 18:11:41 +0630

    Update Enchanted_Project.py

--- Enchanted_Project.py
+++ Enchanted_Project.py
@@ -36,39 +36,11 @@ def add_bg_from_url():
          </style>
          """,
          unsafe_allow_html=True
-     )
-
-def _set_block_container_style(
-    max_width: int = 1200,
-    max_width_100_percent: bool = False,
-    padding_top: int = 1,
-    padding_right: int = 1,
-    padding_left: int = 1,
-    padding_bottom: int = 1,
-    ):
-    if max_width_100_percent:
-        max_width_str = f"max-width: 100%;"
-    else:
-        max_width_str = f"max-width: {max_width}px;"
-        
-    styl = f"""
-    <style>
-        .reportview-container .main .block-container{{
-            {max_width_str}
-            padding-top: {padding_top}rem;
-            padding-right: {padding_right}rem;
-            padding-left: {padding_left}rem;
-            padding-bottom: {padding_bottom}rem;
-        }}
-        }}
-    </style>
-    """
-    st.markdown(styl, unsafe_allow_html=True)    
+     )  
 
 add_bg_from_url()
 
-
-st.header('Color Analysis with David')
+st.title('Taylor Swift Midnights Album Color Analysis with David')
 
 #Read the images and Convert the color to RBG
 #Read the images using imread method by OpenCV to read the image. And then, we are converting the color format from BGR to RGB using cvtColor
