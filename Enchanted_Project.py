4eb6a198fce63a9607d794e5be2af0edaca1a9c1
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-10-27 18:28:36 +0630

    Update Enchanted_Project.py

--- Enchanted_Project.py
+++ Enchanted_Project.py
@@ -30,13 +30,6 @@ def add_bg_from_url():
              background-attachment: fixed;
              background-size: cover
          }}
-         body {
-            font-family: neue-haas-grotesk-display;
-            font-weight: 700;
-            color: #a89bad;
-            background: #525b84;
-            line-height: 1.65;
-        }
          </style>
          """,
          unsafe_allow_html=True
@@ -44,6 +37,17 @@ def add_bg_from_url():
 
 add_bg_from_url()   
 
+st.markdown(
+    <style>
+     body {
+            font-family: neue-haas-grotesk-display;
+            font-weight: 700;
+            color: #a89bad;
+            background: #525b84;
+            line-height: 1.65;
+        }
+    </style>
+)
 st.title('Taylor Swift Midnights Album Vinyl Varients Color Analysis with David')
 
 #Read the images and Convert the color to RBG
