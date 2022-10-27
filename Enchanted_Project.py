0694f0ed50d755479a0bf84902467fb297908be8
ZinMinThaw-23 <69190291+ZinMinThaw-23@users.noreply.github.com>
2022-10-27 17:49:38 +0630

    Update Enchanted_Project.py

--- Enchanted_Project.py
+++ Enchanted_Project.py
@@ -107,7 +107,7 @@ st.image("TS-Midnights-Logo.PNG", width=200)
 col1, col2= st.columns((1,1))
 
 
-
+local_css("style.css")
 with st.container():
     with col1:
         st.image("Moon Stone Blue Vinyl.png", caption="Original Photo", width=720, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
